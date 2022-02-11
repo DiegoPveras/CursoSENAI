22c3089fff4a88857d2fc9603f3092dfa8025182
DiegopVeras <diegopveras02@gmail.com>
2022-02-11 15:17:34 -0300

    first commit

---
+++ senai.js/atvdemaula.js
@@ -0,0 +1,12 @@
+let numAlunos = 20;
+
+for (let i = 0; i < numAlunos; i++){
+    if (i == 0){
+        console.log(`O numero ${i} é zero`)
+    }else if (i %2 == 0){
+        console.log(`U numero ${i} é par`)
+    }else {
+    console.log(`O numero ${i} é impar`)
+    }
+
+}
